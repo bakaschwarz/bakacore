af8796986d9226fb34d6a44a60d6f0d94ab64c98
Yannick Bülter <yannick.buelter@yabue.de>
2014-10-30 15:00:46 +0100

    Neue Klasse begonnen!
    + Klasse ObservableConfiguration angelegt
    + Neben Konstruktoren auch Getter für Objects, Strings und Integer implementiert.

---
+++ src/main/java/de/yabue/bakacore/Configurations/ObservableConfiguration.java
@@ -0,0 +1,133 @@
+package de.yabue.bakacore.Configurations;
+
+import javafx.beans.property.Property;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
+
+import java.net.URL;
+import java.util.HashMap;
+
+/**
+ * Diese Klasse bietet eine Möglichkeit, Werte aus einer {@code .properties} auszulesen und diese in
+ * {@link javafx.beans.value.ObservableValue} zu kapseln.
+ * Man kann sowohl die Property, als auch den Wert selbst anfordern.
+ * Allerdings müssen Nutzer dieser Klasse wissen, um was für eine Art Wert es sich handelt, wenn sie
+ * die Property anfordern.
+ */
+@Slf4j
+public class ObservableConfiguration {
+
+    private final URL CONFIG_PATH;
+
+    private final PropertiesConfiguration PROPERTIES_CONFIGURATION;
+
+    private final HashMap<String, Property> MAP;
+
+    /**
+     * Konstruiert eine neue Konfigurierung anhand einer übergebenen Properties Datei.
+     * @param pathToConfiguration Pfad zu einer Properties Datei.
+     */
+    public ObservableConfiguration(@NonNull URL pathToConfiguration) throws ConfigurationException {
+        CONFIG_PATH = pathToConfiguration;
+        PROPERTIES_CONFIGURATION = new PropertiesConfiguration(pathToConfiguration);
+        MAP = new HashMap<String, Property>();
+        log.debug("Konfiguration initialisiert...");
+    }
+
+    /**
+     * Konstruiert eine leere Konfiguration.
+     */
+    public ObservableConfiguration(){
+        CONFIG_PATH = null;
+        PROPERTIES_CONFIGURATION = new PropertiesConfiguration();
+        MAP = new HashMap<String, Property>();
+        log.debug("Eine leere Konfiguration wurde initialisiert...");
+    }
+
+    /**
+     * Liefert zu einem übergebenen Schlüssel einen observierbaren Wert zurück. Wird keiner gefunden, so wird mit dem
+     * übergebenen Standardwert ein observierbarer Wert erstellt. Diese Methode fügt allerdings keine Werte und Schlüssel
+     * permanent in die Properties Datei ein.
+     * @param keyWord Der Schlüssel, zu dem ein observierbarer Wert gesucht werden soll.
+     * @param defaultValue Ein Wert der genutzt wird, wenn in der Konfigurierungsdatei kein passender Wert oder Schlüssel vorhanden ist.
+     * @param <T> Typ des gewünschten Werts.
+     * @return Eine Referenz auf einen observierbaren Wert aus der Properties Datei, oder auf den Standartwert.
+     */
+    public <T> SimpleObjectProperty<T> getObjectProperty(@NonNull final String keyWord, @NonNull T defaultValue){
+        SimpleObjectProperty result;
+        if(MAP.get(keyWord) == null){
+            if(PROPERTIES_CONFIGURATION.getProperty(keyWord) != null){
+                result = new SimpleObjectProperty<T>((T) PROPERTIES_CONFIGURATION.getProperty(keyWord));
+                log.debug("Schlüssel ist nicht in der Map, füge "+keyWord+" hinzu...");
+            }else{
+                result = new SimpleObjectProperty<T>(defaultValue);
+                log.warn("Schlüssel ist nicht in der Map und der Schlüssel existiert nicht oder hat keinen Wert...");
+                log.info("Füge temporär einen Standartwert für "+keyWord+" ein...");
+            }
+            MAP.put(keyWord, result);
+        }else{
+            result = (SimpleObjectProperty) MAP.get(keyWord);
+            log.debug("Wert für "+keyWord+" gefunden...");
+        }
+        return result;
+    }
+
+    /**
+     * Liefert zu einem übergebenen Schlüssel einen observierbaren Wert zurück. Wird keiner gefunden, so wird mit dem
+     * übergebenen Standardwert ein observierbarer Wert erstellt. Diese Methode fügt allerdings keine Werte und Schlüssel
+     * permanent in die Properties Datei ein.
+     * @param keyWord Der Schlüssel, zu dem ein observierbarer Wert gesucht werden soll.
+     * @param defaultValue Ein Wert der genutzt wird, wenn in der Konfigurierungsdatei kein passender Wert oder Schlüssel vorhanden ist.
+     * @return Eine Referenz auf einen observierbaren Wert aus der Properties Datei, oder auf den Standartwert.
+     */
+    public SimpleStringProperty getStringProperty(@NonNull final String keyWord, @NonNull String defaultValue){
+        SimpleStringProperty result;
+        if(MAP.get(keyWord) == null){
+            if(PROPERTIES_CONFIGURATION.getProperty(keyWord) != null){
+                result = new SimpleStringProperty((String) PROPERTIES_CONFIGURATION.getProperty(keyWord));
+                log.debug("Schlüssel ist nicht in der Map, füge "+keyWord+" hinzu...");
+            }else{
+                result = new SimpleStringProperty(defaultValue);
+                log.warn("Schlüssel ist nicht in der Map und der Schlüssel existiert nicht oder hat keinen Wert...");
+                log.info("Füge temporär einen Standartwert für "+keyWord+" ein...");
+            }
+            MAP.put(keyWord, result);
+        }else{
+            result = (SimpleStringProperty) MAP.get(keyWord);
+            log.debug("Wert für "+keyWord+" gefunden...");
+        }
+        return result;
+    }
+
+    /**
+     * Liefert zu einem übergebenen Schlüssel einen observierbaren Wert zurück. Wird keiner gefunden, so wird mit dem
+     * übergebenen Standardwert ein observierbarer Wert erstellt. Diese Methode fügt allerdings keine Werte und Schlüssel
+     * permanent in die Properties Datei ein.
+     * @param keyWord Der Schlüssel, zu dem ein observierbarer Wert gesucht werden soll.
+     * @param defaultValue Ein Wert der genutzt wird, wenn in der Konfigurierungsdatei kein passender Wert oder Schlüssel vorhanden ist.
+     * @return Eine Referenz auf einen observierbaren Wert aus der Properties Datei, oder auf den Standartwert.
+     */
+    public SimpleIntegerProperty getIntegerProperty(@NonNull final String keyWord, @NonNull Integer defaultValue){
+        SimpleIntegerProperty result;
+        if(MAP.get(keyWord) == null){
+            if(PROPERTIES_CONFIGURATION.getProperty(keyWord) != null){
+                result = new SimpleIntegerProperty((Integer) PROPERTIES_CONFIGURATION.getProperty(keyWord));
+                log.debug("Schlüssel ist nicht in der Map, füge "+keyWord+" hinzu...");
+            }else{
+                result = new SimpleIntegerProperty(defaultValue);
+                log.warn("Schlüssel ist nicht in der Map und der Schlüssel existiert nicht oder hat keinen Wert...");
+                log.info("Füge temporär einen Standartwert für "+keyWord+" ein...");
+            }
+            MAP.put(keyWord, result);
+        }else{
+            result = (SimpleIntegerProperty) MAP.get(keyWord);
+            log.debug("Wert für "+keyWord+" gefunden...");
+        }
+        return result;
+    }
+}
