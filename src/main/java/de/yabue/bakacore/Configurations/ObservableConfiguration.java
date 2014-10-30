3a03d8888141da2124a752a15decfe6504c07491
Yannick Bülter <yannick.buelter@yabue.de>
2014-10-30 23:04:50 +0100

    Finished??

--- src/main/java/de/yabue/bakacore/Configurations/ObservableConfiguration.java
+++ src/main/java/de/yabue/bakacore/Configurations/ObservableConfiguration.java
@@ -35,7 +35,7 @@ public class ObservableConfiguration {
      * @param pathToConfiguration Pfad zu einer Properties Datei.
      * @param autoSave Wenn {@code true}, werden Änderungen an den Properties gespeichert.
      */
-    public ObservableConfiguration(@NonNull URL pathToConfiguration, @NonNull boolean autoSave) throws ConfigurationException {
+    public ObservableConfiguration(@NonNull URL pathToConfiguration, boolean autoSave) throws ConfigurationException {
         CONFIG_PATH = pathToConfiguration;
         PROPERTIES_CONFIGURATION = new PropertiesConfiguration(pathToConfiguration);
         PROPERTIES_CONFIGURATION.setAutoSave(autoSave);
@@ -57,34 +57,6 @@ public class ObservableConfiguration {
         PROPERTIES_CONFIGURATION = new PropertiesConfiguration();
         MAP = new HashMap<String, Property>();
         log.debug("Eine leere Konfiguration wurde initialisiert...");
-    }
-
-    /**
-     * Liefert zu einem übergebenen Schlüssel einen observierbaren Wert zurück. Wird keiner gefunden, so wird mit dem
-     * übergebenen Standardwert ein observierbarer Wert erstellt. Diese Methode fügt allerdings keine Werte und Schlüssel
-     * permanent in die Properties Datei ein.
-     * @param keyWord Der Schlüssel, zu dem ein observierbarer Wert gesucht werden soll.
-     * @param defaultValue Ein Wert der genutzt wird, wenn in der Konfigurierungsdatei kein passender Wert oder Schlüssel vorhanden ist.
-     * @param <T> Typ des gewünschten Werts.
-     * @return Eine Referenz auf einen observierbaren Wert aus der Properties Datei, oder auf den Standartwert.
-     */
-    public <T> SimpleObjectProperty<T> getObjectProperty(@NonNull final String keyWord, @NonNull T defaultValue){
-        SimpleObjectProperty result;
-        if(MAP.get(keyWord) == null){
-            if(PROPERTIES_CONFIGURATION.getProperty(keyWord) != null){
-                result = new SimpleObjectProperty<T>((T) PROPERTIES_CONFIGURATION.getProperty(keyWord));
-                log.debug("Schlüssel ist nicht in der Map, füge "+keyWord+" hinzu...");
-            }else{
-                result = new SimpleObjectProperty<T>(defaultValue);
-                log.warn("Schlüssel ist nicht in der Map und der Schlüssel existiert nicht oder hat keinen Wert...");
-                log.info("Füge temporär einen Standartwert für "+keyWord+" ein...");
-            }
-            MAP.put(keyWord, result);
-        }else{
-            result = (SimpleObjectProperty) MAP.get(keyWord);
-            log.debug("Wert für "+keyWord+" gefunden...");
-        }
-        return result;
     }
 
     /**
@@ -149,7 +121,7 @@ public class ObservableConfiguration {
      * @param defaultValue Ein Wert der genutzt wird, wenn in der Konfigurierungsdatei kein passender Wert oder Schlüssel vorhanden ist.
      * @return Eine Referenz auf einen observierbaren Wert aus der Properties Datei, oder auf den Standartwert.
      */
-    public SimpleDoubleProperty getDoubleProperty(@NonNull final String keyWord, @NonNull Integer defaultValue){
+    public SimpleDoubleProperty getDoubleProperty(@NonNull final String keyWord, @NonNull Double defaultValue){
         SimpleDoubleProperty result;
         if(MAP.get(keyWord) == null){
             if(PROPERTIES_CONFIGURATION.getProperty(keyWord) != null){
@@ -168,5 +140,65 @@ public class ObservableConfiguration {
         return result;
     }
 
+    /**
+     * Setzt einen Wert für ein übergebenes Schlüsselwort.
+     * @param keyWord Der Schlüssel, zu dem ein observierbarer Wert zugewiesen werden soll.
+     * @param value Der Wert, der dem Schlüssel zugewiesen werden soll.
+     */
+    public SimpleStringProperty setStringProperty(@NonNull final String keyWord, @NonNull final String value){
+        SimpleStringProperty property;
+        if( PROPERTIES_CONFIGURATION.getProperty(keyWord) == null){
+            property = new SimpleStringProperty(value);
+            MAP.put(keyWord, property);
+        }else{
+            property = getStringProperty(keyWord, value);
+        }
+        PROPERTIES_CONFIGURATION.setProperty(keyWord, value);
+        log.debug("Setze neuen Wert für "+keyWord+"...");
+        return property;
+    }
 
+    /**
+     * Setzt einen Wert für ein übergebenes Schlüsselwort.
+     * @param keyWord Der Schlüssel, zu dem ein observierbarer Wert zugewiesen werden soll.
+     * @param value Der Wert, der dem Schlüssel zugewiesen werden soll.
+     */
+    public SimpleIntegerProperty setIntegerProperty(@NonNull final String keyWord, @NonNull final Integer value){
+        SimpleIntegerProperty property;
+        if( PROPERTIES_CONFIGURATION.getProperty(keyWord) == null){
+            property = new SimpleIntegerProperty(value);
+            MAP.put(keyWord, property);
+        }else{
+            property = getIntegerProperty(keyWord, value);
+        }
+        PROPERTIES_CONFIGURATION.setProperty(keyWord, value);
+        log.debug("Setze neuen Wert für "+keyWord+"...");
+        return property;
+    }
+
+    /**
+     * Setzt einen Wert für ein übergebenes Schlüsselwort.
+     * @param keyWord Der Schlüssel, zu dem ein observierbarer Wert zugewiesen werden soll.
+     * @param value Der Wert, der dem Schlüssel zugewiesen werden soll.
+     */
+    public SimpleDoubleProperty setDoubleProperty(@NonNull final String keyWord, @NonNull final Double value){
+        SimpleDoubleProperty property;
+        if( PROPERTIES_CONFIGURATION.getProperty(keyWord) == null){
+            property = new SimpleDoubleProperty(value);
+            MAP.put(keyWord, property);
+        }else{
+            property = getDoubleProperty(keyWord, value);
+        }
+        PROPERTIES_CONFIGURATION.setProperty(keyWord, value);
+        log.debug("Setze neuen Wert für "+keyWord+"...");
+        return property;
+    }
+
+    /**
+     * Setzen dieses Wertes entscheidet, ob Änderungen an der Properties Datei permanent sind, oder nicht.
+     * @param autoSave {@code true}, wenn Änderungen gespeichert werden sollen.
+     */
+    public void setAutoSaveProperty(final boolean autoSave){
+        autoSaveProperty.setValue(autoSave);
+    }
 }
